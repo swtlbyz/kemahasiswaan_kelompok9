11442a151281ec549641299bbef6e96cc9df881d
ikhsan15 <ikhsan15nur@gmail.com>
2020-12-01 11:48:51 +0700

    Pull Request #2 Ikhsan to Bayu

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:kemahasiswaan_kelompok9/view/linkbutton.dart';
+import 'package:kemahasiswaan_kelompok9/pages/home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-// test push
-//test lagiii
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 100.0),
-                child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    LinkButton(Colors.blue),
-                    Row(
-                      children: [
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(top: 10.0, bottom: 20.0),
-                          child: Text(
-                            "Pengumuman",
-                            style: TextStyle(color: Colors.blue, fontSize: 18),
-                          ),
-                        ),
-                      ],
-                    ),
-                    LinkButton(Colors.blue),
-                    Row(
-                      children: [
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(top: 10.0, bottom: 20.0),
-                          child: Text(
-                            "Data",
-                            style: TextStyle(color: Colors.blue, fontSize: 18),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 100.0),
-                child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    LinkButton(Colors.blue),
-                    Row(
-                      children: [
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(top: 10.0, bottom: 20.0),
-                          child: Text(
-                            "Beastudi",
-                            style: TextStyle(color: Colors.blue, fontSize: 18),
-                          ),
-                        ),
-                      ],
-                    ),
-                    LinkButton(Colors.blue),
-                    Row(
-                      children: [
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(top: 10.0, bottom: 20.0),
-                          child: Text(
-                            "Organisasi",
-                            style: TextStyle(color: Colors.blue, fontSize: 18),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+      title: 'flutter demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: Home(),
     );
   }
 }
---
+++ lib/pages/akademik.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Akademik extends StatefulWidget {
+  @override
+  _AkademikState createState() => _AkademikState();
+}
+
+class _AkademikState extends State<Akademik> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('akademik'),
+      ),
+    );
+  }
+}
